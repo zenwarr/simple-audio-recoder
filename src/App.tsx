3e5f4b13fca00cacf5968b6f58b919ddfeba8fe2
zenwarr <zenw@yandex.ru>
2022-12-02 20:04:44 +0600

    allow selecting media device

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createSignal, For, onMount } from "solid-js";
 import styles from "./App.module.css";
 
 
@@ -12,8 +12,26 @@ export function AudioRecorder() {
 
   const rec = () => recorder.isRecording();
 
+  onMount(() => recorder.loadDevices());
+
   return <>
     <div class={ styles.container }>
+      <select class={ styles.select } value={ recorder.getSelectedDevice()?.deviceId }
+              onChange={ e => recorder.selectDevice(e.currentTarget.value) }
+              disabled={ recorder.isRecording() }>
+        <For<MediaDeviceInfo> each={ recorder.getDevices() }>
+          {
+            (d: MediaDeviceInfo) => <option value={ d.deviceId }>{ d.label }</option>
+          }
+        </For>
+      </select>
+
+      <div class={ styles.audioContainer }>
+        {
+            recorder.getAudioUrl() != null && <audio src={ recorder.getAudioUrl() } controls autoplay/>
+        }
+      </div>
+
       <button onClick={ () => !rec() ? recorder.start() : recorder.stop() }
               class={ styles.button }
               classList={ { [styles.buttonRecording]: rec() } }>
@@ -21,12 +39,6 @@ export function AudioRecorder() {
           rec() ? "Stop Recording" : "Start Recording"
         }
       </button>
-
-      {
-          recorder.getAudioUrl() != null && <div class={ styles.audioContainer }>
-          <audio src={ recorder.getAudioUrl() } controls autoplay/>
-        </div>
-      }
     </div>
   </>;
 }
@@ -37,12 +49,28 @@ class Record {
   private _setAudioUrl: (url: string | undefined) => void;
   private _recording: () => boolean;
   private _setRecording: (value: boolean) => void;
+  private _devices: () => MediaDeviceInfo[];
+  private _setDevices: (d: MediaDeviceInfo[]) => void;
+  private _selectedDevice: () => MediaDeviceInfo | undefined;
+  private _setSelectedDevice: (d: MediaDeviceInfo | undefined) => void;
   private recorder: MediaRecorder | undefined;
 
 
   constructor() {
     [ this._recording, this._setRecording ] = createSignal(false);
     [ this._audioUrl, this._setAudioUrl ] = createSignal();
+    [ this._devices, this._setDevices ] = createSignal([]);
+    [ this._selectedDevice, this._setSelectedDevice ] = createSignal(undefined);
+  }
+
+
+  async loadDevices() {
+    this._setDevices((await navigator.mediaDevices.enumerateDevices()).filter(d => d.kind === "audioinput"));
+  }
+
+
+  getDevices() {
+    return this._devices();
   }
 
 
@@ -56,8 +84,25 @@ class Record {
   }
 
 
+  getSelectedDevice() {
+    return this._selectedDevice();
+  }
+
+
+  selectDevice(deviceId: string) {
+    this._setSelectedDevice(this._devices().find(d => d.deviceId === deviceId));
+  }
+
+
   async start() {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        autoGainControl: false,
+        echoCancellation: false,
+        noiseSuppression: false,
+        deviceId: this._selectedDevice()?.deviceId || "default"
+      }
+    });
     this.recorder = new MediaRecorder(stream);
     this.recorder.start();
     this._setRecording(true);
